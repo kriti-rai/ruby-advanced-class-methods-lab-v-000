e9bddc201aa64a63ffcbb395f8a20bd841d9aa77
Kriti Rai <kriti.rai@gmail.com>
2018-02-04 01:38:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,9 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
+  def self.create
     song = self.new
-    song.name = name
+    # song.name = name
     self.all << song
     song
   end
