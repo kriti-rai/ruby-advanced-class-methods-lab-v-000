6ab415ccf6ddcc4f4102a494b197cc4d9d853d40
Kriti Rai <kriti.rai@gmail.com>
2018-02-04 02:08:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,16 @@ class Song
   def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name
+    else
+      self.create_by_name(name)
+    end
+  end
+      
+      
+    
+  end
 end
